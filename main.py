0ac3a43e0ece2ecd3d733f40bd047cb2f03a0af0
Marko Vučković <markovucko12@gmail.com>
2025-01-29 18:11:20 +0100

    Add Cart class and functionality to add products and calculate total value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from product import Product
 from product_manager import ProductManager
+from cart import Cart
 
 # Kreiranje proizvoda
 
@@ -16,3 +17,14 @@ manager.add_product(product2)
 
 print(manager.show_products())
 print(f"Total inventory value: ${manager.total_inventory_value()}")
+
+# Kreiranje korpe i didavanje proizvoda u korpu
+
+cart = Cart()
+cart.add_to_cart(product1)
+cart.add_to_cart(product2)
+
+# Prikaz proizvoda u korpi i ukupne vrednosti
+
+print(cart.show_cart())
+print(f"Total cart value: ${cart.total_cart_value()}")
